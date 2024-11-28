66c79a78be44d1d00b29c763b6617618afabe8b6
ashkan <ashkan@beigi.net>
2024-11-28 00:05:13 -0500

    .

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pyhackrf_tx',
+    name='pyhackrf-tx',
     version='0.2.1',  # Update the version number
     description='Python interface for HackRF with transmitter support',
     author='Ash Beigi',
     author_email='ash@qoherent.ai',
-    url='https://github.com/qoherent/pyhackrf_tx',
+    url='https://github.com/qoherent/pyhackrf-tx',
     packages=find_packages(),
     install_requires=[
         'numpy',
